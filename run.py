3312df8cc2e4dfe435d939c16f7cd817a5401458
goz <goz@host.com>
2023-02-01 09:36:23 +0100

    add milvus

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from crawler import Crawler
 from encoder import Encoder
 import pudb
 import argparse
+from milvus import setup_milvus
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("root_directory")
@@ -9,7 +11,7 @@ parser.add_argument("root_directory")
 args = parser.parse_args()
 
 
-
+setup_milvus()
 crawler = Crawler(args.root_directory, ["jpg"])
 
 files = crawler.start()
